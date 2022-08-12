4f855226bca2052109cd8c69e025c498c7e1db9c
Gandhi-V <velasquez.gandhi@gmail.com>
2022-08-12 14:55:46 -0700

    Script to reset USB drivers

--- usb-scripts/reset-usb.sh
+++ usb-scripts/reset-usb.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+#This is the location where the usb are binded.
+#This program works with just one specific USB input.
+#In this case is 0000:00:0c.0 from personal mouse.
 base="/sys/bus/pci/drivers"
+sleep_secs="5"
+
 
 echo "Looking for USBs ..."
 for usb_std in "$base/"?hci*
-do
-	echo "* USB standard '$usb_std' ..."
-	for dev_path in "$usb_std/"*:*
-	do
-		dev="$(basename "$dev_path")"
-		echo "  -  Resetting device '$dev' ..."
-	done
+do  
+	#If the directory matches the mouse location then we unbind. 
+	if [[ $usb_std = "$base/xhci_hcd" ]]
+	then 
+		echo "Removing drivers..."
+		#This program needs sudo permission unless we are on root.
+		echo 0000:00:0c.0  | sudo tee $usb_std/unbind
+		#Waits for 3 secs to Re-connect drivers.
+		echo "Drivers has been removed."
+		sleep "$sleep_secs"
+		echo "Binding drivers..."
+		echo 0000:00:0c.0  | sudo tee $usb_std/bind
+		sleep "$sleep_secs"
+		echo "Drivers has been connected."
+	fi
 done
-echo "Done."
-
+echo "Process Done."
